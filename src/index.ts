d2a48e9cf8288234538fb6f235df0c6740f29366
Jan Schutte <jan.o.schutte@gmail.com>
2020-05-07 18:30:31 +0200

    Add math rendering

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import './style.sass'
+import katex from 'katex'
 
 const a: string = 'Hello World!'
 console.log(a)
+
+
+const math_blocks = document.querySelectorAll('div.math')
+
+math_blocks.forEach((e) => {
+    if (e.hasAttribute('math')){
+        const math = e.getAttribute('math')
+        katex.render(math, e, {
+            output: 'mathml',
+            fleqn: false
+        })
+    }
+})
+
