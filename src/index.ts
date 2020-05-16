aef61ab45046b44c052282ccddacb6af2c65d0e9
Jan Schutte <jan.o.schutte@gmail.com>
2020-05-16 19:53:31 +0200

    New colors + Search svg

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import './style.sass'
 
-console.log('Hello there')
+// Setup search functionality
+const search_btn: HTMLSpanElement = document.querySelector('#search_btn')
+const search_field: HTMLInputElement = document.querySelector('#search_field')
+
+search_field.classList.add('hide')
+search_btn.addEventListener('click', (event) => {
+    if (search_field.classList.contains('hide')) {
+        search_field.classList.remove('hide')
+    } else {
+        search_field.classList.add('hide')
+    }
+    
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
                 use: ['file-loader']
             }
         ],
