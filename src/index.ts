04c9700cbb063fe98dce4b45991c6c5f44aac14a
Jan Schutte <jan.o.schutte@gmail.com>
2020-05-26 23:52:25 +0200

    Add search index

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import './style.sass'
 const search_btn: HTMLSpanElement = document.querySelector('#search_btn')
 const search_field: HTMLInputElement = document.querySelector('#search_field')
 
-search_field.classList.add('hide')
 search_btn.addEventListener('click', (event) => {
     if (search_field.classList.contains('hide')) {
         search_field.classList.remove('hide')
