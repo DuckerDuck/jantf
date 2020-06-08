726df96655de01ed9a81943bcc0a39b3bbf1cd5b
Jan Schutte <jan.o.schutte@gmail.com>
2020-06-08 22:38:41 +0200

    Automatically focus search field

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const search_results: HTMLDivElement = document.querySelector('#search_result')
 search_btn.addEventListener('click', (event) => {
     if (search_field.classList.contains('hide')) {
         search_field.classList.remove('hide')
+        search_field.focus()
     } else {
         search_field.classList.add('hide')
         search_results.innerHTML = ''
